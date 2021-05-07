7c2e3f59ab42baa6fc77c7a19705e1b3bfa2e073
Daniel Laufer <lauferkdaniel@gmail.com>
2021-05-07 15:48:59 -0400

    changed some variable names and added docstrings

--- code/box.py
+++ code/box.py
@@ -1,7 +1,21 @@
 from datetime import datetime
 
-STATUS_COLORS = {"EMPTY": (255, 255, 255), "WALL": (0, 0, 0), "START": (50, 205, 50), "END": (255, 0, 0), 'PROCESSED': (255, 0, 255), 'TO_PROCESS': (128, 0, 255)}
+STATUS_COLORS = {
+    "EMPTY": (255, 255, 255), 
+    "WALL": (0, 0, 0), 
+    "START": (50, 205, 50), 
+    "END": (255, 0, 0), 
+    'VISITED': (255, 0, 255), 
+    'TO_VISIT': (128, 0, 255)
+}
+
 class Box:
+    """
+    An individual box on the screen.
+
+
+
+    """
     def __init__(self, x: int, y: int, width: int, height: int, row: int, col: int) -> None:
         self.color = (255, 255, 255)
         self.status = "EMPTY"
@@ -17,7 +31,12 @@ class Box:
         self.neighbors = []
         self.parent = None
 
+
     def toggle_wall_status(self) -> None:
+        """
+        If this box is currently a wall vertex, make it an empty vertex.
+        If this box is currently an empty vertex, make it a wall vertex.
+        """
         if self.status != "WALL":
             self.status = "WALL"
             self.color = STATUS_COLORS['WALL']
@@ -26,6 +45,10 @@ class Box:
         self.color = STATUS_COLORS['EMPTY']
 
     def toggle_start_status(self) -> None:
+        """
+        If this box is currently a start vertex, make it an empty vertex.
+        If this box is currently an empty vertex, make it a start vertex.
+        """
         if self.status != "START":
             self.status = "START"
             self.color = STATUS_COLORS['START']
@@ -34,6 +57,10 @@ class Box:
         self.color = STATUS_COLORS['EMPTY']
 
     def toggle_end_status(self) -> None:
+        """
+        If this box is currently an end vertex, make it an empty vertex.
+        If this box is currently an empty vertex, make it an end vertex.
+        """
         if self.status != "END":
             self.status = "END"
             self.color = STATUS_COLORS['END']
@@ -41,11 +68,17 @@ class Box:
         self.status = "EMPTY"
         self.color = STATUS_COLORS['EMPTY']
 
-    def toggle_processed(self) -> None:
+    def toggle_visisted(self) -> None:
+        """
+        Mark this box as 'visited' meaning our algorithm has dealt with this.
+        """
         self.status = 'PROCESSED'
         self.color = STATUS_COLORS['PROCESSED']
 
-    def toggle_to_process(self) -> None:
+    def toggle_to_visit(self) -> None:
+        """
+        Mark this box as 'to_visit' meaning our algorithm is going to visit this box.
+        """
         self.status == 'TO_PROCESS'
         self.color = STATUS_COLORS['TO_PROCESS']
 
@@ -56,7 +89,3 @@ class Box:
 
     def __repr__(self) -> str:
         return '({}, {})'.format(self.row, self.col)
-
-
-
-
