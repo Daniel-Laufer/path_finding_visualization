ca8e9954cd69bacd7bd8e82ea91f69e593205818
Daniel Laufer <lauferkdaniel@gmail.com>
2021-05-08 16:05:31 -0400

    implemented the Queue ADT which will be used in BFS

---
+++ code/ADTs.py
@@ -0,0 +1,23 @@
+from typing import *
+
+
+class Queue():
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self) -> bool:
+        return len(self.items) == 0
+
+    def enqueue(self, item: Any) -> None:
+        self.items.append(item)
+
+    def dequeue(self) -> Any:
+        if self.is_empty():
+            raise Exception
+        return self.items.pop(0)
+
+    def empty_queue(self) -> None:
+        while not self.is_empty():
+            self.dequeue()
+
